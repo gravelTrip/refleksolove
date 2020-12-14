50e3d9cf6813e8f625a3378dcbcec77db595d300
Michal Chmiel <mchmielo@gmail.com>
2020-12-14 17:37:31 +0100

    finish v1.0

--- script/script.js
+++ script/script.js
@@ -1,22 +1,26 @@
+//Green modal
 const btnGreen = document.querySelector('.specialization__article-btn--green');
+const modalGreen = document.querySelector('.modal--green');
+btnGreen.addEventListener('click', () => modalGreen.classList.add('active'));
+const closeIconGreen = document.querySelector('.modal__icon--green');
+const closeBtnGreen = document.querySelector('.modal__btn-close--green');
+closeIconGreen.addEventListener('click', () => modalGreen.classList.remove('active'));
+closeBtnGreen.addEventListener('click', () => modalGreen.classList.remove('active'));
+
+//Gold modal
 const btnGold = document.querySelector('.specialization__article-btn--gold');
+const modalGold = document.querySelector('.modal--gold');
+btnGold.addEventListener('click', () => modalGold.classList.add('active'));
+const closeIconGold = document.querySelector('.modal__icon--gold');
+const closeBtnGold = document.querySelector('.modal__btn-close--gold');
+closeIconGold.addEventListener('click', () => modalGold.classList.remove('active'));
+closeBtnGold.addEventListener('click', () => modalGold.classList.remove('active'));
+
+//Gold dark
 const btnDark = document.querySelector('.specialization__article-btn--dark');
-const closeIcon = document.querySelector('.modal__icon');
-const modal = document.querySelector('.modal');
-const closeBtn = document.querySelector('.modal__btn-close');
-
-
-btnGreen.addEventListener('click', active);
-btnGold.addEventListener('click', active);
-btnDark.addEventListener('click', active);
-
-closeIcon.addEventListener('click', inActive);
-closeBtn.addEventListener('click', inActive);
-
-function active() {
-  modal.classList.add('active');
-}
-
-function inActive() {
-  modal.classList.remove('active');
-}
+const modalDark = document.querySelector('.modal--dark');
+btnDark.addEventListener('click', () => modalDark.classList.add('active'));
+const closeIconDark = document.querySelector('.modal__icon--dark');
+const closeBtnDark = document.querySelector('.modal__btn-close--dark');
+closeIconDark.addEventListener('click', () => modalDark.classList.remove('active'));
+closeBtnDark.addEventListener('click', () => modalDark.classList.remove('active'));
